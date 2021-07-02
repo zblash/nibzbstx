16b80b988f998463e2048c889680ea9b083b3cad
Yusuf Can Çelik <yusufcancelik@hotmail.com>
2021-07-02 11:07:40 +0300

    store registry changed

--- src/generateStore.js
+++ src/generateStore.js
@@ -1,23 +1,43 @@
 import { store } from "./store";
+export const StoreRegistry = (function () {
+  let storeInst;
 
-export function generateStore(...reducerWithActions) {
-  let _initialState = {};
-  const forwards = [];
-  const asyncForwards = [];
-  let generatedActions = {};
-  let generatedAsyncActions = {};
-  reducerWithActions.forEach((reducerWithAction) => {
-    _initialState = Object.assign(_initialState, reducerWithAction.getState());
-    forwards.push(reducerWithAction.syncForward);
-    asyncForwards.push(reducerWithAction.asyncForward);
-    Object.assign(generatedActions, reducerWithAction.actions);
-    Object.assign(generatedAsyncActions, reducerWithAction.asyncActions);
-  });
-  return store(
-    _initialState,
-    forwards,
-    generatedActions,
-    asyncForwards,
-    generatedAsyncActions
-  );
-}
+  function register(...reducerWithActions) {
+    let _initialState = {};
+    const forwards = [];
+    const asyncForwards = [];
+    let generatedActions = {};
+    let generatedAsyncActions = {};
+    reducerWithActions.forEach((reducerWithAction) => {
+      _initialState = Object.assign(
+        _initialState,
+        reducerWithAction.getState()
+      );
+      forwards.push(reducerWithAction.syncForward);
+      asyncForwards.push(reducerWithAction.asyncForward);
+      Object.assign(generatedActions, reducerWithAction.actions);
+      Object.assign(generatedAsyncActions, reducerWithAction.asyncActions);
+    });
+
+    if (storeInst) {
+      storeInst.registerState(
+        _initialState,
+        forwards,
+        generatedActions,
+        asyncForwards,
+        generatedAsyncActions
+      );
+    } else {
+      storeInst = store(
+        _initialState,
+        forwards,
+        generatedActions,
+        asyncForwards,
+        generatedAsyncActions
+      );
+    }
+    return storeInst;
+  }
+
+  return { register };
+})();
